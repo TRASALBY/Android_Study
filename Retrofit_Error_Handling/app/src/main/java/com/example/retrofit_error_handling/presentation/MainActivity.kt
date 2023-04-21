34dbe4bd3ee9b3faa9b1cc2901dfe7b3166a0b46
TRASALBY <ghtjd8264@naver.com>
2023-04-21 17:21:39 +0900

    feat: 레트로핏 관련 의존성 주입

--- Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/presentation/MainActivity.kt
+++ Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.example.retrofit_error_handling
+package com.example.retrofit_error_handling.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.retrofit_error_handling.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
