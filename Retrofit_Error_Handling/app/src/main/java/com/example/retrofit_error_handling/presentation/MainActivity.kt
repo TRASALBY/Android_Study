c2425325e33217b19766e58d523b671d9d659e11
TRASALBY <ghtjd8264@naver.com>
2023-04-21 17:51:55 +0900

    feat: 어댑터 연결 및 뷰모델 관찰

--- Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/presentation/MainActivity.kt
+++ Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/presentation/MainActivity.kt
@@ -1,18 +1,42 @@
 package com.example.retrofit_error_handling.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.example.retrofit_error_handling.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.retrofit_error_handling.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var bookAdapter: BookAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setBookRecyclerView()
+    }
+
+
+    private fun setBookRecyclerView() {
+        bookAdapter = BookAdapter()
+        binding.rvBookList.adapter = bookAdapter
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                mainViewModel.bookList.collectLatest {
+                    bookAdapter.submitList(it)
+                }
+            }
+        }
     }
 
 }
--- Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/presentation/MainViewModel.kt
+++ Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/presentation/MainViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.retrofit_error_handling.presentation
 
 import androidx.lifecycle.ViewModel
+import com.example.retrofit_error_handling.presentation.model.BookUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
+
+    private val _bookList: MutableStateFlow<List<BookUiModel>> = MutableStateFlow(emptyList())
+    val bookList = _bookList.asStateFlow()
+
+    fun getBookList() {
+        // 도서 요청
+    }
 }
