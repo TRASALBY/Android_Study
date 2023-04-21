ba76735955338b7cc6a9152d4c16a6239ad87fe0
TRASALBY <ghtjd8264@naver.com>
2023-04-21 17:20:58 +0900

    feat: 책 검색 api 구성

---
+++ Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/data/model/Book.kt
@@ -0,0 +1,8 @@
+package com.example.retrofit_error_handling.data.model
+
+data class Book(
+    val title: String,
+    val contents: String,
+    val url: String,
+    val price: Int
+)
---
+++ Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/data/service/KakaoBookApi.kt
@@ -0,0 +1,14 @@
+package com.example.retrofit_error_handling.data.service
+
+import com.example.retrofit_error_handling.data.model.Book
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface KakaoBookApi {
+    @GET("/b3/search/book")
+    fun searchBook(
+        @Query("query")
+        query: String
+    ):Response<Book>
+}
