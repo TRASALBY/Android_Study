9b8e34b7533e49d8027bf495aac3149c434626e3
TRASALBY <ghtjd8264@naver.com>
2023-04-21 17:28:12 +0900

    feat: 뷰모델 생성

--- Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/presentation/MainActivity.kt
+++ Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/presentation/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.retrofit_error_handling.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.retrofit_error_handling.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/presentation/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.retrofit_error_handling.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+}
