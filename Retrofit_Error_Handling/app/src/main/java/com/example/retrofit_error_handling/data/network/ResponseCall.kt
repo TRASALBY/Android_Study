7cb21772382add6693106556c8e3afc845b8acd1
TRASALBY <ghtjd8264@naver.com>
2023-04-23 18:38:06 +0900

    feat: 에러 메시지 파싱

--- Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/data/network/ResponseCall.kt
+++ Retrofit_Error_Handling/app/src/main/java/com/example/retrofit_error_handling/data/network/ResponseCall.kt
@@ -1,7 +1,9 @@
 package com.example.retrofit_error_handling.data.network
 
 import com.example.retrofit_error_handling.data.error.NullBodyException
+import com.example.retrofit_error_handling.data.model.ErrorResponse
 import com.example.retrofit_error_handling.data.model.Result
+import com.google.gson.Gson
 import okhttp3.Request
 import okio.Timeout
 import retrofit2.Call
@@ -12,37 +14,56 @@ import java.io.IOException
 class ResponseCall<T>(
     private val callDelegate: Call<T>
 ) : Call<Result<T>> {
+
+    val gson = Gson()
     override fun clone(): Call<Result<T>> = ResponseCall(callDelegate.clone())
 
     override fun execute(): Response<Result<T>> =
         throw UnsupportedOperationException("ResponseCall does not support execute.")
+
     override fun isExecuted() = callDelegate.isExecuted
     override fun cancel() = callDelegate.cancel()
     override fun isCanceled() = callDelegate.isCanceled
     override fun request(): Request = callDelegate.request()
     override fun timeout(): Timeout = callDelegate.timeout()
-    override fun enqueue(callback: Callback<Result<T>>) = callDelegate.enqueue(object : Callback<T> {
-        override fun onResponse(call: Call<T>, response: Response<T>) {
-            val body = response.body()
+    override fun enqueue(callback: Callback<Result<T>>) =
+        callDelegate.enqueue(object : Callback<T> {
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                val body = response.body()
 
-            if(response.isSuccessful) {
-                if(body != null){
-                    callback.onResponse(this@ResponseCall, Response.success(Result.Success(body)))
+                if (response.isSuccessful) {
+                    if (body != null) {
+                        callback.onResponse(
+                            this@ResponseCall,
+                            Response.success(Result.Success(body))
+                        )
+                    } else {
+                        callback.onResponse(
+                            this@ResponseCall,
+                            Response.success(Result.Unexpected(NullBodyException("body가 null임")))
+                        )
+                    }
                 } else {
-                    callback.onResponse(this@ResponseCall, Response.success(Result.Unexpected(NullBodyException("body가 null임"))))
+                    val errorMessage = runCatching {
+                        gson.fromJson(
+                            response.errorBody()?.string(),
+                            ErrorResponse::class.java
+                        ).message
+                    }.getOrElse { "Unknown Error" }
+                    callback.onResponse(
+                        this@ResponseCall,
+                        Response.success(Result.Failure(response.code(), errorMessage))
+                    )
                 }
-            } else {
-                callback.onResponse(this@ResponseCall, Response.success(Result.Failure(response.code(), response.message())))
             }
-        }
 
-        override fun onFailure(call: Call<T>, t: Throwable) {
-            val result : Result<T> = when(t) {
-                is IOException -> Result.NetworkError(t)
-                else -> Result.Unexpected(t)
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                val result: Result<T> = when (t) {
+                    is IOException -> Result.NetworkError(t)
+                    else -> Result.Unexpected(t)
+                }
+                callback.onResponse(this@ResponseCall, Response.success(result))
             }
-            callback.onResponse(this@ResponseCall,Response.success(result))
-        }
 
-    })
+        })
 }
