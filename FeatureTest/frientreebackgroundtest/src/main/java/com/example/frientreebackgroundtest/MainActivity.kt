56c4767f173f7b01f1baa09442ac16c87e1321ff
이호성 <ghtjd8264@naver.com>
2024-01-15 17:59:58 +0900

    feat: 배경에 glide사용해 이미지

--- FeatureTest/frientreebackgroundtest/src/main/java/com/example/frientreebackgroundtest/MainActivity.kt
+++ FeatureTest/frientreebackgroundtest/src/main/java/com/example/frientreebackgroundtest/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.frientreebackgroundtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ivTree = findViewById<ImageView>(R.id.iv_tree)
+
+        Glide.with(this).load(R.raw.tree_background).into(ivTree)
     }
 }
