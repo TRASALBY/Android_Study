f3aa2b8ef14857eb602d9fa2cc3e0934abc4831c
이호성 <ghtjd8264@naver.com>
2024-01-16 22:49:09 +0900

    녹음 기능 1차 테스트

--- FeatureTest/micrecordmultipart/src/main/java/com/example/micrecordmultipart/MainActivity.kt
+++ FeatureTest/micrecordmultipart/src/main/java/com/example/micrecordmultipart/MainActivity.kt
@@ -11,15 +11,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.airbnb.lottie.LottieAnimationView
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mediaRecorder: MediaRecorder
+    private var mediaRecorder: MediaRecorder? = null
     private var isRecording = false
     private val RECORD_AUDIO_PERMISSION_CODE = 123
 
     private lateinit var recordBtn: Button
-    private lateinit var recordLottie : LottieAnimationView
+    private lateinit var recordLottie: LottieAnimationView
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,13 +34,15 @@ class MainActivity : AppCompatActivity() {
 
         recordBtn.setOnClickListener {
 
-            if(isRecording.not()){
+            if (isRecording.not()) {
                 //녹음 시작
                 recordLottie.playAnimation()
+                startRecording()
                 recordBtn.text = "녹음 중단"
-            } else{
+            } else {
                 recordLottie.progress = 0f
                 recordLottie.pauseAnimation()
+                stopRecording()
                 recordBtn.text = "녹음 시작"
             }
 
@@ -51,15 +54,38 @@ class MainActivity : AppCompatActivity() {
 
     private fun requestAudioPermission() {
         if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.RECORD_AUDIO
+                this, Manifest.permission.RECORD_AUDIO
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.RECORD_AUDIO),
-                RECORD_AUDIO_PERMISSION_CODE
+                this, arrayOf(Manifest.permission.RECORD_AUDIO), RECORD_AUDIO_PERMISSION_CODE
             )
         }
+    }
+
+    private fun startRecording() {
+        mediaRecorder = MediaRecorder()
+
+        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+        // 내부 저장소에 파일 생성
+        val filePath = "${filesDir}/audio_record.m4a"
+        mediaRecorder?.setOutputFile(filePath)
+
+        try {
+            mediaRecorder?.prepare()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        mediaRecorder?.start()
+    }
+
+    private fun stopRecording() {
+        mediaRecorder?.stop()
+        mediaRecorder?.release()
+        mediaRecorder = null
     }
 }
