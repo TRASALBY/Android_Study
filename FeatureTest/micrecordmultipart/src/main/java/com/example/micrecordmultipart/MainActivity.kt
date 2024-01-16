7bc95e1cd214f5d126f09b544e9c9f36022ff7e9
이호성 <ghtjd8264@naver.com>
2024-01-16 22:13:58 +0900

    오디오 권환 확인

--- FeatureTest/micrecordmultipart/src/main/java/com/example/micrecordmultipart/MainActivity.kt
+++ FeatureTest/micrecordmultipart/src/main/java/com/example/micrecordmultipart/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.micrecordmultipart
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mediaRecorder: MediaRecorder
+    private var isRecording = false
+    private val RECORD_AUDIO_PERMISSION_CODE = 123
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestAudioPermission()
+
+
+    }
+
+    private fun requestAudioPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                RECORD_AUDIO_PERMISSION_CODE
+            )
+        }
     }
 }
