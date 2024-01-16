78df954fab20d5c5734266e71e9d09dc22247f3b
이호성 <ghtjd8264@naver.com>
2024-01-16 22:28:50 +0900

    녹음 관련 간단 뷰 디자인

--- FeatureTest/micrecordmultipart/src/main/java/com/example/micrecordmultipart/MainActivity.kt
+++ FeatureTest/micrecordmultipart/src/main/java/com/example/micrecordmultipart/MainActivity.kt
@@ -5,10 +5,12 @@ import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Build
 import android.os.Bundle
+import android.widget.Button
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,12 +18,33 @@ class MainActivity : AppCompatActivity() {
     private var isRecording = false
     private val RECORD_AUDIO_PERMISSION_CODE = 123
 
+    private lateinit var recordBtn: Button
+    private lateinit var recordLottie : LottieAnimationView
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         requestAudioPermission()
+
+        recordBtn = findViewById(R.id.btn_record)
+        recordLottie = findViewById(R.id.lottieAnimationView)
+
+        recordBtn.setOnClickListener {
+
+            if(isRecording.not()){
+                //녹음 시작
+                recordLottie.playAnimation()
+                recordBtn.text = "녹음 중단"
+            } else{
+                recordLottie.progress = 0f
+                recordLottie.pauseAnimation()
+                recordBtn.text = "녹음 시작"
+            }
+
+            isRecording = isRecording.not()
+        }
 
 
     }
